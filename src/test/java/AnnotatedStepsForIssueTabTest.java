ceb869d9c1c9d2dafef5418b7b2c6552586f30f1
Roman Chesanov <roman.chesanov>
2022-03-06 22:23:58 +0300

    Annotated Test + WebStepsDescription added

--- src/test/java/AnnotatedStepsForIssueTabTest.java
+++ src/test/java/AnnotatedStepsForIssueTabTest.java
@@ -1,2 +1,21 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
 public class AnnotatedStepsForIssueTabTest {
+
+    public static final String REPO_NAME_2 = "ChesKnow/qa_guru_Ches_Allure_1lesson";
+
+    @Test
+    public void annotatedStepsForIssueTab() {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        WebStepsDescription websteps = new WebStepsDescription();
+        websteps.openMainPage();
+        websteps.searchForRepoByName(REPO_NAME_2);
+        websteps.openRepositoryByLink(REPO_NAME_2);
+        websteps.checkIfTabIssueVisibleInRepo();
+        websteps.takeScreenshot();
+    }
 }
--- src/test/java/WebStepsDescription.java
+++ src/test/java/WebStepsDescription.java
@@ -1,2 +1,37 @@
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import static com.codeborne.selenide.Condition.visible;
+
 public class WebStepsDescription {
+
+    @Step("Открываем страницу браузера \"https://github.com\"")
+    public void openMainPage() {
+        Selenide.open("https://github.com/");}
+
+    @Step("Находим поисковую строку и вставляем в неё название репозитория {repo_name}")
+    public void searchForRepoByName(String repo_name) {
+        Selenide.$("[data-test-selector='nav-search-input']").
+                setValue("ChesKnow/qa_guru_Ches_Allure_1lesson").pressEnter();
+    }
+
+    @Step("в результатах поиска находим и нажимаем ссылку {repo_name}")
+    public void openRepositoryByLink(String repo_name) {
+        Selenide.$(By.linkText("ChesKnow/qa_guru_Ches_Allure_1lesson")).click();
+    }
+
+    @Step("Проверяем, что в репозитории есть таб Issue")
+    public void checkIfTabIssueVisibleInRepo() {
+        Selenide.$("#issues-tab").should(visible);
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
 }
